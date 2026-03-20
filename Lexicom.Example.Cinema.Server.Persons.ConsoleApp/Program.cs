17b61ce98e35989ccf7a4aeb4b104610cbf0c2db
AlexLexicon <Alexrs.business@outlook.com>
2026-03-20 14:22:46 -0700

    improved and updated launch settings to be easier to use

--- Lexicom.Example.Cinema.Server.Persons.ConsoleApp/Program.cs
+++ Lexicom.Example.Cinema.Server.Persons.ConsoleApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lexicom.AspNetCore.Controllers.Extensions;
+using Lexicom.ConsoleApp.DependencyInjection;
+using Lexicom.ConsoleApp.Tui.Extensions;
+using Lexicom.DependencyInjection.Primitives.Extensions;
+using Lexicom.DependencyInjection.Primitives.For.ConsoleApp.Extensions;
+using Lexicom.Example.Cinema.Server.Persons.Application.Extensions;
+using Lexicom.Example.Cinema.Server.Persons.ConsoleApp;
+using Lexicom.Example.Cinema.Server.Persons.Database.Extensions;
+using Lexicom.Logging.ConsoleApp.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+/*
+ * Lexicom.Example.Cinema.Server.Persons.ConsoleApp
+ */
+
+ConsoleApplicationBuilder builder = ConsoleApplication.CreateBuilder();
+
+builder.Lexicom(options =>
+{
+    options.AddLogging();
+    options.AddTui<AssemblyScanMarker>();
+    options.AddPrimitives(options =>
+    {
+        options.AddTimeProvider();
+        options.AddGuidProvider();
+    });
+});
+
+builder.Services.AddPersonsDatabase();
+builder.Services.AddPersonsApplication();
+
+ConsoleApplication app = builder.Build();
+
+await app.RunLexicomTuiAsync();
